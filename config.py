47bc40806170355f12a6d83c889abb60492dbe55
Desha_Elfager <96248074+Desha-elfager@users.noreply.github.com>
2022-08-08 03:18:55 +0200

    Update config.py

--- config.py
+++ config.py
@@ -9,9 +9,9 @@ if os.path.exists(".env"):
     load_dotenv(".env")
 
 # Necessary Vars
-API_ID = int(os.getenv("API_ID" "12339829"))
-API_HASH = os.getenv("API_HASH" "685177285c55dd5f9a1f9bba0e443bce")
-SESSION = os.getenv("SESSION" "BACLdIxRzzMypw1rX8vcGlNNjOWVS4KNB3vTGv2OHjffuh5S-80DuEqJobNa2eDTggbTAYoXyUrqaBLw9YbYw8sryOxGGkY6aDyh0f__eA5rlrMLDjywz6F4tVCKwoe4gCGchMwwPY_Xmx5c_QkxIdfi38mxGMDV3QP-6FDfQDt2v1RO0zy7g6101g5hSkaAYgqP2tZm9sGaFDaX4eqNJjlBwUr3lp4aO4PmwE-cLINWVIDuzpE60GVdqRZEjZg1KFKvkXYtt221PWlmvEUtzhiVf6Ok_P0fgiT9ig9hMyLZ3r4Cy91-hjnw6DrwxPifoPWyJ-NuClEi5WtGepTBVjtUewl7hwA")
+API_ID = int(os.getenv("API_ID"))
+API_HASH = os.getenv("API_HASH")
+SESSION = os.getenv("SESSION")
 HNDLR = os.getenv("HNDLR", "/")
 SUDO_USERS = list(map(int, os.getenv("SUDO_USERS").split()))
 
